9de0de19d6beed171b3246ec7a31f7b949d6941a
Kauê Gomes <kauesgomes@hotmail.com>
2025-05-07 08:41:00 -0300

    fix(Avaliacao): Corrige nomes de propriedades no construtor para refletir a nomenclatura correta

--- src/model/avaliacao.js
+++ src/model/avaliacao.js
@@ -1,13 +1,14 @@
 export default class Avaliacao {
     constructor(avaliacaoId, reservaId, nota, comentario) {
-        this.id = id;
-        this.idReserva = idReserva;
+        this.avaliacaoId = avaliacaoId;
+        this.reservaId = reservaId;
         this.nota = nota;
         this.comentario = comentario;
     }
 }
 
 export class AvaliacaoNota {
+    static ZERO = 0;
     static UM = 1;
     static DOIS = 2;
     static TRES = 3;
